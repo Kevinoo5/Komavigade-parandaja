d6eecfc8dd65ca6fb5f736ffdc92bbdb709788dc
Kevinoo5 <87882945+Kevinoo5@users.noreply.github.com>
2021-11-18 04:01:16 +0200

    Lisab sõnadele "et", "sest" ja "kuid" komad

---
+++ komad.py
@@ -0,0 +1,8 @@
+def paranda_komavead(lause):
+    lause = lause.replace(" sest ", ", sest ")
+    lause = lause.replace(" et ", ", et ")
+    lause = lause.replace(" kuid ", ", kuid ")
+    lause = lause.replace(" siis ", ", siis ")
+    print(lause)
+paranda_komavead("Ma olen sinine sest et ma olen punane siis kui ma ei ole roheline")
+    
