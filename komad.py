4ee585894d39f9382ec714f1dafcc0156b200648
Kevinoo5 <87882945+Kevinoo5@users.noreply.github.com>
2021-11-18 17:09:12 +0200

    Add files via upload

--- komad.py
+++ komad.py
@@ -3,6 +3,6 @@ def paranda_komavead(lause):
     lause = lause.replace(" et ", ", et ")
     lause = lause.replace(" kuid ", ", kuid ")
     lause = lause.replace(" siis ", ", siis ")
+    lause = lause.replace(" mis ", ", mis " )
     print(lause)
 paranda_komavead("Ma olen sinine sest et ma olen punane siis kui ma ei ole roheline")
-    
