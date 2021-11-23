2965315c5ac760641c3e87cacb530dd60e38ae90
Kevinoo5 <87882945+Kevinoo5@users.noreply.github.com>
2021-11-23 15:05:18 +0200

    Lisasin faili avamise

--- komad.py
+++ komad.py
@@ -4,5 +4,13 @@ def paranda_komavead(lause):
     lause = lause.replace(" kuid ", ", kuid ")
     lause = lause.replace(" siis ", ", siis ")
     lause = lause.replace(" mis ", ", mis " )
-    print(lause)
-paranda_komavead("Ma olen sinine sest et ma olen punane siis kui ma ei ole roheline")
+    lause = lause.replace(" aga ", ", aga ")
+    return lause
+# paranda_komavead("Ma olen sinine sest et ma olen punane siis kui ma ei ole roheline")
+fail1 = open("vigane.txt", encoding  = "UTF-8")
+fail2 = open("parandatud.txt", "w", encoding = "UTF-8")
+for line in fail1:
+    parandus = paranda_komavead(line)
+    fail2.write(parandus)
+fail1.close()
+fail2.close()
